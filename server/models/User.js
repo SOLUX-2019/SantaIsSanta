37c4d0867abd4043e5d4e6cae5a10f38f2c626f5
mjms0214 <mjms0214@sookmyung.ac.kr>
2022-01-17 19:49:22 +0900

    비밀번호 암호화

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,7 @@
 const { ObjectId } = require('bson');
 const mongoose = require('mongoose');
+const  bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name:{
@@ -40,6 +42,22 @@ const userSchema = mongoose.Schema({
         type:Number
     }
     })
+
+userSchema.pre('save', function(next){
+    var user = this;
+    
+    if (user.isModified('password')){
+        // 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            });
+        });
+    }
+})
 
 const User = mongoose.model('User', userSchema)
 module.exports = { User }
