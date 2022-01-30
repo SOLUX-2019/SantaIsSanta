5d0701f417a074144220b8a3259c91ad2f489a28
hannaa <sshanna29@sookmyung.ac.kr>
2022-01-30 21:03:23 +0900

    회원가입

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,7 +5,7 @@ let auth = (req, res, next) => { //인증 처리
     //쿠키에서 토큰 가져오기
     let token = req.cookies.x_auth;
 
-    // 토큰을 복호화 한후  유저를 찾는다.
+    // 토큰을 복호화 한후  유저를 찾기
     User.findByToken(token, (err, user) => {
         if (err) throw err;
         if (!user) return res.json({ isAuth: false, error: true })
