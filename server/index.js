e072d1fe9fe2e9050f3bcd9d53d414392aefc1c0
hannaa <sshanna29@sookmyung.ac.kr>
2022-02-03 04:11:29 +0900

    회원가입정보저장

--- server/index.js
+++ server/index.js
@@ -17,10 +17,10 @@ const corsOptions = {
 
 // application/x-www-form-urlendcoded 형식의 데이터를
 // 가져와서 분석할 수 있도록
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true}))
 // application/json 타입의 데이터를
 // 가져와서 분석할 수 있도록
-app.use(bodyParser.json())
+app.use(express.json())
 app.use(cookieParser());
 app.use(cors(corsOptions))
 
@@ -37,8 +37,8 @@ MongoClient.connect(config.mongoURI, function (err, client) {
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
-   // useNewUrlParser: true,
-  //  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
@@ -48,14 +48,25 @@ app.post('/api/user/register', (req, res) => {
     // 그것들을 디비에 넣음
 
     const user = new User(req.body)
-    user.save((err, userInfo) => {
+
+    /*  user.save((err, userInfo) => {
         if(err) {
             return res.json({success: false, err})
         }
         return res.status(200).json({
             success: true
         })
-    })
+    })*/
+
+    db.collection('user').insertOne(user, (err, user) =>{
+        if(err) {
+            console.log(err)
+            return res.json({success: false, err})
+        }
+        return res.status(200).json({
+            success: true
+        })
+        })   
 })
 
 app.post('/api/user/login', (res, req)=>{
