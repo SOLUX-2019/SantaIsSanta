da4d2e7ea5977baebf6e471ed81dde83d8477ad6
seoin-cho <choseoin1140@sookmyung.ac.kr>
2022-02-05 13:30:27 +0900

    feat: 로그인 회원가입 state 설정

--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -1,97 +1,26 @@
-import React from 'react';
-import styled from "styled-components";
+import React, { useState } from 'react';
+import { Container, Header, Form, Footer } from './styledLogin';
 import Axios from 'axios';
 import { Link } from "react-router-dom";
 import "../../src/assets/font/font.css";
 
 const Login = () => {
-    const Container = styled.div`
-        background-color: #F5F6F8;
-        width: 100%;
-        height: 100vh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    `;
+    const [id, setId] = useState("");
+    const [password, setPassword] = useState("");
 
-    const Header = styled.h1`
-        font-size: 3em;
-        font-family: "Pretendard";
-    `;
+    const submit = () => {
+        alert(`Welcome ${id}`);
+        window.location.href='/';
+    };
 
-    const Form = styled.form`
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-
-        input {
-            width: 30%;
-            height: 50px;
-            font-size: 1.2em;
-            padding: 12px 0px;
-            margin-top: 20px;
-            border: 1px solid lightgray;
-            border-radius: 4px;
-        }
-
-        button {
-            background-color: #1E4620;
-            color: white;
-            font-weight: bold;
-            font-family: "Pretendard";
-            width: 30%;
-            font-size: 1.5em;
-            padding: 17px 0px;
-            margin-top: 40px;
-            border: 1px solid #1E4620;
-            border-radius: 4px;
-            text-align: center;
-            text-decoration: none;
-
-            &:hover {
-                cursor: pointer;
-            }
-        }
-    `;
-
-    const Footer = styled.div`
-        width: 30%;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-evenly;
-        align-items: center;
-        padding: 20px 0px;
-        
-        div {
-            font-size: 0.9em;
-            font-family: "Pretendard";
-        }
-        
-        .link {
-            font-size: 0.9em;
-            font-family: "Pretendard";
-            text-decoration: none;
-            color: black;
-
-            &:hover {
-                font-weight: bold;
-                text-decoration: underline;
-                cursor: pointer;
-            }
-        }
-        
-    `;
-    
     return (
         <div className="Login">
             <Container>
                 <Header>로그인</Header>
                 <Form>
-                    <input name="userId" type="text" placeholder="아이디" />
-                    <input name="password" type="password" placeholder="비밀번호" />
-                    <button type="button" onClick={() => window.location.href='/'}>로그인</button>
+                    <input name="userId" type="text" placeholder="아이디" onChange={(e)=> setId(e.target.value)}/>
+                    <input name="password" type="password" placeholder="비밀번호" onChange={(e)=> setPassword(e.target.value)}/>
+                    <button type="button" onClick={submit}>로그인</button>
                 </Form>
                 <Footer>
                     <div>계정이 없으신가요?</div>
