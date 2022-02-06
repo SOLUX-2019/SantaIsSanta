914dbdd611781f3856b978e94478466ba56d4851
seoin-cho <choseoin1140@sookmyung.ac.kr>
2022-02-06 10:42:54 +0900

    refactor: styledInfoPage 파일 추가

--- client/src/Mountain/InfoPage/MountainLoc.js
+++ client/src/Mountain/InfoPage/MountainLoc.js
@@ -1,35 +1,10 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import { Loc, PreviousPage, LastPage } from './styledInfoPage';
 import { FaMountain, FaAngleRight } from "react-icons/fa";
 import "../../assets/font/font.css";
 
 const MountainLoc = ({region, mid}) => {
-    
-    const Loc = styled.div`
-        margin-top: 40px;
-        display: flex;
-        align-items: center;
-    `;
-
-    const PreviousPage = styled.span`
-        font-family: "Pretendard";
-
-        :hover {
-            cursor: pointer;
-        }
-
-        .link {
-            text-decoration: none;
-            color: black;
-        }
-    `;
-
-    const LastPage = styled.span`
-        font-weight: bold;
-        font-family: "Pretendard";
-    `;
-
 
     return (
         <Loc>
