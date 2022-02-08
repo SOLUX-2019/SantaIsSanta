66738a58a9561b2c4a6f9374e276a16a86180c91
lizuAg <wndms058@gmail.com>
2022-02-08 23:34:23 +0900

    마이페이지 로딩

--- client/src/MyPage/MyPage.js
+++ client/src/MyPage/MyPage.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaPencilAlt } from "react-icons/fa";
 import "../../src/assets/font/font.css";
 import { Wrap, LeftContainer, RightContainer } from "./styledMyPage";
@@ -12,11 +12,17 @@ import BadgeCollection from "./BadgeCollection";
 const MyPage = () => {
   const [user, setUser] = useState({});
   const [postList, setPostList] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     Axios.get("/api/user/info")
       .then((res) => {
-        setUser(res.data);
+        /*console.log(res.data);
+        if (!res.data.success) {
+          alert("로그인 후 이용해 주세요.");
+          navigate("/login");
+        }
+        setUser(res.data);*/
       })
       .catch((err) => {
         console.log(err);
@@ -33,20 +39,24 @@ const MyPage = () => {
 
   return (
     <>
-      <Wrap>
-        <LeftContainer>
-          <img src={user.img ? user.img : defaultImg} alt={"프로필 사진"} />
-          <Link className="goEdit" to="/modifyProfile">
-            회원정보 수정
-            <FaPencilAlt />
-          </Link>
-          <Profile user={user} />
-        </LeftContainer>
-        <RightContainer>
-          <BadgeCollection postCount={postList.length} />
-          <PostList postList={postList} />
-        </RightContainer>
-      </Wrap>
+      {user == {} ? (
+        <Wrap>
+          <LeftContainer>
+            <img src={user.img ? user.img : defaultImg} alt={"프로필 사진"} />
+            <Link className="goEdit" to="/modifyProfile">
+              회원정보 수정
+              <FaPencilAlt />
+            </Link>
+            <Profile user={user} />
+          </LeftContainer>
+          <RightContainer>
+            <BadgeCollection postCount={postList.length} />
+            <PostList postList={postList} />
+          </RightContainer>
+        </Wrap>
+      ) : (
+        ""
+      )}
     </>
   );
 };
