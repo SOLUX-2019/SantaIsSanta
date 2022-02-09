8681250533176d60ef3ca29941ec54161cf176dc
lizuAg <wndms058@gmail.com>
2022-02-10 05:24:57 +0900

    feat/lading page 변경

--- client/src/LandingPage/LandingPage.js
+++ client/src/LandingPage/LandingPage.js
@@ -1,32 +1,43 @@
-import styled from "styled-components";
 import TextComponent from "./text";
 import WeatherInfo from "./WeatherInfo";
-import logo from "../assets/img/logo_titleO.png";
-
-const Body = styled.div`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-
-  img {
-    width: 30%;
-    height: 30%;
-    border: 0.5px solid grey;
-    padding: 5px;
-    margin: 50px;
-    box-shadow: 5px 5px 5px grey;
-  }
-`;
+import logo from "../assets/img/landing/santaBlack.png";
+import { Slide, Fade, AttentionSeeker } from "react-awesome-reveal";
+import { FaAngleRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { Page1Wrap, Page2Wrap, Gap } from "./styledLandingPage";
 
 const LandingPage = () => {
   return (
-    <div>
-      <Body>
-        <img src={logo} alt={"로고 이미지"} />
-        <TextComponent />
-      </Body>
-      <WeatherInfo />
-    </div>
+    <>
+      <Page1Wrap>
+        <Fade triggerOnce={false} direction={"up"} cascade delay={10}>
+          <h1>
+            <b>MZ세대</b>를 위한 등산 커뮤니티
+          </h1>
+        </Fade>
+        <Fade triggerOnce={false} direction={"up"} cascade delay={1010}>
+          <img src={logo} alt="산타"></img>
+        </Fade>
+      </Page1Wrap>
+      <Gap></Gap>
+      <Page2Wrap>
+        <Fade triggerOnce={false} direction={"up"} cascade>
+          <h2>
+            산타는 산타와 함께 친구들을 만나고
+            <br /> 등산과 관련된 이야기를 나눠요 !<br />
+          </h2>
+          <Link to="community">
+            <b>커뮤니티 페이지로</b>
+            <FaAngleRight />
+          </Link>
+          <h2>산타는 산타가 추천하는 산</h2>
+          <Link to="/mountain/region?region=%EC%88%98%EB%8F%84%EA%B6%8C">
+            <b>수도권 대표 산 정보 보러가기</b>
+            <FaAngleRight />
+          </Link>
+        </Fade>
+      </Page2Wrap>
+    </>
   );
 };
 export default LandingPage;
---
+++ client/src/LandingPage/styledLandingPage.js
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+import bg1 from "../assets/img/landing/001.png";
+import bg2 from "../assets/img/landing/004.png";
+
+export const Page1Wrap = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-image: url(${bg2});
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  background-attachment: fixed;
+  display: flex;
+
+  flex-direction: column;
+  text-align: center;
+  color: black;
+
+  font-family: "Leferi";
+  font-weight: lighter;
+  h1 {
+    margin-top: 50px;
+  }
+  h2 {
+    margin-top: 60px;
+  }
+  img {
+    width: 300px;
+    height: 300px;
+  }
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`;
+export const Filter = styled.div`
+width: inherit;
+height: inherit;
+  background-color: rgba(0, 0, 0, 0.3)
+}
+`;
+
+export const Page2Wrap = styled.div`
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  background-image: url(${bg1});
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+
+  display: flex;
+  text-align: center;
+  color: black;
+
+  font-family: "Leferi";
+  font-weight: lighter;
+  h1 {
+    margin-top: 50px;
+  }
+  h2 {
+    margin-top: 60px;
+  }
+  a {
+    display: flex;
+    justify-content: center;
+    color: black;
+    text-decoraion: none;
+    align-items: center;
+  }
+  svg {
+    width: 25px;
+    height: 25px;
+  }
+`;
+
+export const Gap = styled.div`
+  width: 100%;
+  height: 20vh;
+    background-color: #b8e9ff;
+  );
+`;
