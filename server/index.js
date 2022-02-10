f2068c6518f29591016e9f9b1265011f6974ff64
mjms0214 <mjms0214@sookmyung.ac.kr>
2022-02-10 09:48:47 +0900

    게시글 검색 코드 수정

--- server/index.js
+++ server/index.js
@@ -250,7 +250,7 @@ app.delete("/community/post/delete/:id", (req, res) => {
 
 // 게시글 검색
 app.get("/community/post/search", (req, res) => {
-    Post.find({title: req.body.title, cateory: req.body.cateory}, (err, post_search) => {
+    Post.find({title: req.query["title"], category: req.query["category"]}, (err, post_search) => {
         if (!post_search) {
         console.log("검색 키워드에 해당하는 게시글이 없습니다.");
         return res.json({
